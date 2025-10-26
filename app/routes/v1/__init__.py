81c7d4584e92fe8d7b55d8ec2b711648da7928b4
AchmadMuafiTaufiqurrochman <withdraw14@gmail.com>
2025-10-26 22:14:54 +0700

    Include accounts router in v1 API router for enhanced routing functionality

--- app/routes/v1/__init__.py
+++ app/routes/v1/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from .auth_router import router as auth_router
+from .accounts_router import router as accounts_router
 
 router_v1 = APIRouter()
 router_v1.include_router(auth_router)
+router_v1.include_router(accounts_router)
