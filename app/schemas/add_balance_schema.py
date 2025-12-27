5476a7f02b0d3e7369db8c68185f010aac593204
AchmadMuafiTaufiqurrochman <withdraw14@gmail.com>
2025-12-27 17:45:02 +0700

    Enhance schemas by adding example values and descriptions for better validation and documentation

--- app/schemas/add_balance_schema.py
+++ app/schemas/add_balance_schema.py
@@ -1,4 +1,4 @@
 from pydantic import BaseModel, Field
 
 class AddBalanceSchema(BaseModel):
-    amount: float = Field(..., gt=0, description="Jumlah saldo yang ingin ditambahkan")
+    amount: float = Field(..., gt=0, example=100000, description="Jumlah saldo yang ingin ditambahkan")
--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -4,11 +4,11 @@ from datetime import date
 
 class RegisterRequest(BaseModel):
     full_name: str = Field(..., example="Budi Santoso")
-    birth_date: date
-    address: str
+    birth_date: date = Field(..., example="1990-04-23")
+    address: str = Field(..., example="Jl. Merdeka No. 10, Jakarta")
     nik: str = Field(..., example="3201012304950007")
-    phone_number: str
-    email: EmailStr
-    username: str
-    password: str
-    PIN: str
+    phone_number: str = Field(..., example="6281234567890")
+    email: EmailStr = Field(..., example="budi.santoso@example.com")
+    username: str = Field(..., example="budisantoso")
+    password: str = Field(..., example="securepassword123")
+    PIN: str = Field(..., example="123456")
